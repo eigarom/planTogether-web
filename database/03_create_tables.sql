6b943062014709555e687319d2fd17b63b1131b2
LaplanteC <laplantec.msi@gmail.com>
2024-10-09 16:22:51 -0400

    corr bdd

--- database/03_create_tables.sql
+++ database/03_create_tables.sql
@@ -27,6 +27,7 @@ CREATE TABLE family
 (
     id_family              serial PRIMARY KEY,
     name                   varchar NOT NULL,
+    color                  varchar NOT NULL,       
     image_content          bytea,
     image_content_type     text,
     invite_code            varchar UNIQUE,
@@ -85,6 +86,7 @@ CREATE TABLE period
 
 CREATE TABLE alert
 (
+    id_alert  serial PRIMARY KEY,
     date_time timestamp PRIMARY KEY,
     id_event  int NOT NULL REFERENCES event (id_event) ON DELETE CASCADE
 );
