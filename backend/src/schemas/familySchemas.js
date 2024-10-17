f01dbef7135ea40a095c338055cce6a2fcdf9868
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-10-17 14:13:06 -0400

    feat (backend): modification familySchema

--- backend/src/schemas/familySchemas.js
+++ backend/src/schemas/familySchemas.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
 
 const familySchema = Joi.object({
-	name: Joi.string().trim().alphanum().required(),
-	color: Joi.string().trim().required(),
+	name: Joi.string().trim().alphanum().max(50).required(),
+	color: Joi.string().max(10).trim().required(),
 	imageContent: Joi.string().trim(),
-    imageContentType: Joi.string().trim(),
+    imageContentType: Joi.string().max(10).trim(),
 });
 
 module.exports = {
