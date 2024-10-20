d44facc5172a288fa33e905157d19f62b6a2f5e8
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-10-20 19:48:38 -0400

    chore(frontend): modificatio authSchemas.js #2

--- frontend/src/schemas/authSchemas.js
+++ frontend/src/schemas/authSchemas.js
@@ -6,20 +6,23 @@ export const registrationSchema = Joi.object({
 		.required()
 		.messages({
 			'string.email': `Le courriel n'est pas valide`
-		}), password: Joi.string()
+		}),
+	password: Joi.string()
 		.pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*]).{16,}$'))
 		.required()
 		.max(50)
 		.messages({
 			'string.pattern.base': 'Le mot de passe doit contenir au moins 16 caractères, une majuscule, une' +
 				' minuscule, un chiffre et un caractère spécial'
-		}), repeat_password: Joi.any()
+		}),
+	repeat_password: Joi.any()
 		.valid(Joi.ref('password'))
 		.required()
 		.messages({
 			'any.only': 'Les mots de passe doivent être identiques'
-		}), name: Joi.string()
-		.pattern(new RegExp('^[a-zA-Z0-9-]+$'))
+		}),
+	name: Joi.string()
+		.pattern(new RegExp('^[a-zA-Z0-9- ]+$'))
 		.required()
 		.max(50)
 		.messages({
