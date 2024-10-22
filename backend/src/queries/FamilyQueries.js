5c552b7e1095461b9d84f6d1df56079648b25620
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-10-22 14:41:29 -0400

    feat(backend): suppression récupération images

--- backend/src/queries/FamilyQueries.js
+++ backend/src/queries/FamilyQueries.js
@@ -2,56 +2,46 @@ const pool = require("../queries/dbPool");
 const MemberQueries = require("./MemberQueries");
 
 class FamilyQueries {
-    static async createFamily(family, userId) {
-        const client = await pool.connect();
+	static async createFamily(family, userId) {
+		const client = await pool.connect();
 
-        try {
-            await client.query("BEGIN");
-            const familyId = await this.insertFamily(family, client);
-            await MemberQueries.updateMemberFamilyId(
-                userId,
-                familyId,
-                client
-            );
-            await client.query("COMMIT");
-            return familyId;
-        } catch (err) {
-            await client.query("ROLLBACK");
-            throw err;
-        } finally {
-            client.release();
-        }
-    }
+		try {
+			await client.query("BEGIN");
+			const familyId = await this.insertFamily(family, client);
+			await MemberQueries.updateMemberFamilyId(
+				userId,
+				familyId,
+				client
+			);
+			await client.query("COMMIT");
+			return familyId;
+		} catch (err) {
+			await client.query("ROLLBACK");
+			throw err;
+		} finally {
+			client.release();
+		}
+	}
 
-    static async insertFamily(family,client) {
-        const result = await pool.query(
-            `INSERT INTO family(
-            name, 
-            color, 
-            image_content, 
-            image_content_type
-            )
-            VALUES ($1, $2, $3, $4)
-            RETURNING id_family`,
-            [
-                family.name,
-                family.color,
-                family.imageContent,
-                family.imageContentType,
-            ]
-        );
-        return result.rows[0].id_family;
-    }
+	static async getFamilyById(familyId) {
+		const result = await pool.query(
+			`SELECT name, color
+             FROM family
+             WHERE id_family = $1`,
+			[familyId]
+		);
+		return result.rows[0];
+	}
 
-    static async getFamilyById(familyId) {
-        const result = await pool.query(
-            `SELECT name, color, image_content, image_content_type
-            FROM family
-            WHERE id_family = $1`,
-            [familyId]
-        );
-         return result.rows[0];
-    }
+	static async insertFamily(family, client) {
+		const result = await pool.query(
+			`INSERT INTO family(name, color)
+             VALUES ($1, $2)
+             RETURNING id_family`,
+			[family.name, family.color]
+		);
+		return result.rows[0].id_family;
+	}
 }
 
 module.exports = FamilyQueries;
