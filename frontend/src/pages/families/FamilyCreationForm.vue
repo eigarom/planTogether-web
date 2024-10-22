809e0d65c2e4d677133c4d6b313507803303f913
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-10-22 14:43:29 -0400

    feat(frontend): Modification apparence création famille

--- frontend/src/pages/families/FamilyCreationForm.vue
+++ frontend/src/pages/families/FamilyCreationForm.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="flex align-items-center justify-content-center min-h-screen">
-        <div class="flex flex-column ">
-            <h1 class="align-self-center">Créer une famille</h1>
-            <form id="familycreationForm" class="flex flex-column row-gap-3 align-content-center w-20rem"
-                @submit.prevent="submitCreateFamily">
+	<div class="flex align-items-center justify-content-center min-h-screen">
+		<div class="flex flex-column ">
+			<h1 class="align-self-center">Créer une famille</h1>
+			<form id="familycreationForm" class="flex flex-column row-gap-3 align-content-center w-20rem"
+				  @submit.prevent="submitCreateFamily">
 
 
-                <FloatLabel variant="in">
-                    <InputText id="email" v-model.trim="name" class="w-20rem" />
-                    <label for="email">Nom</label>
-                </FloatLabel>
+				<FloatLabel variant="on">
+					<InputText id="name" v-model.trim="name" class="w-20rem"/>
+					<label for="name">Nom</label>
+				</FloatLabel>
 
 
-                <div class="flex justify-content-between">
-                    <label for="color">Couleur de la famille </label>
-                    <ColorPicker v-model="color" inputId="color" format="hex" />
-                </div>
+				<div class="flex justify-content-between">
+					<label for="color">Couleur des événements communs </label>
+					<ColorPicker v-model="color" format="hex" inputId="color"/>
+				</div>
 
-                <!-- <Message v-if="errorMessage" class="error-message" severity="error">{{ errorMessage }}</Message> -->
+				<!-- <Message v-if="errorMessage" class="error-message" severity="error">{{ errorMessage }}</Message> -->
 
-                <Button :disabled="isCreateFamilyDisabled" label="Confirmer" type="submit" />
-            </form>
-        </div>
-    </div>
+				<Button :disabled="isCreateFamilyDisabled" label="Confirmer" type="submit"/>
+			</form>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -31,51 +31,51 @@ import Button from "primevue/button";
 import Message from 'primevue/message';
 import FloatLabel from "primevue/floatlabel";
 import ColorPicker from 'primevue/colorpicker';
-import { createFamily } from "@/services/familyServices.js";
-import { createFamilySchema } from "@/schemas/familySchemas.js";
+import {createFamily} from "@/services/familyServices.js";
+import {createFamilySchema} from "@/schemas/familySchemas.js";
 
 export default {
-    components: {
-        InputText, Button, Message, FloatLabel, ColorPicker
-    },
-    data: () => {
-        return {
-            name: "",
-            color: "#358be6",
-            errorMessage: ""
-        };
-    },
-    computed: {
-        isCreateFamilyDisabled() {
-            return !this.name
-        }
-    },
-    methods: {
-        async submitCreateFamily() {
-            this.errorMessage = "";
-            const familyInformations = {
-                name: this.name
-            }
-            const { error } = createFamilySchema.validate(familyInformations);
-            if (error) {
-                this.errorMessage = error.message;
-                return
-            }
-            const newFamily = {
-                name: this.name,
-                color: "#" + this.color,
-                imageContent: "",
-                imageContentType: ""
-            }
-            try {
-                const token = this.$cookies.get('jwtToken');
-                await createFamily(newFamily, token);
-                window.location.href = '/';
-            } catch (err) {
-                this.errorMessage = "Échec de la création de la famille.";
-                console.error("An error occurred:", err);
-            }
-        }
-    }
+	components: {
+		InputText, Button, Message, FloatLabel, ColorPicker
+	},
+	data: () => {
+		return {
+			name: "",
+			color: "#358be6",
+			errorMessage: ""
+		};
+	},
+	computed: {
+		isCreateFamilyDisabled() {
+			return !this.name
+		}
+	},
+	methods: {
+		async submitCreateFamily() {
+			this.errorMessage = "";
+			const familyInformations = {
+				name: this.name
+			}
+			const {error} = createFamilySchema.validate(familyInformations);
+			if (error) {
+				this.errorMessage = error.message;
+				return
+			}
+			const newFamily = {
+				name: this.name,
+				color: "#" + this.color,
+				imageContent: "",
+				imageContentType: ""
+			}
+			try {
+				const token = this.$cookies.get('jwtToken');
+				await createFamily(newFamily, token);
+				window.location.href = '/';
+			} catch (err) {
+				this.errorMessage = "Échec de la création de la famille.";
+				console.error("An error occurred:", err);
+			}
+		}
+	}
 };
 </script>
