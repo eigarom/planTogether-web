e4a617906f10f00de8a4aa2d0c0687b9a6d6c398
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-10-31 14:30:12 -0400

    Update SidebarNavigation #6
    Envoyer createInvitation et copyInviteCode dans FamilyProfile

--- frontend/src/components/SidebarNavigation.vue
+++ frontend/src/components/SidebarNavigation.vue
@@ -1,18 +1,17 @@
 <template>
-	<Menu v-if="!isLoading" id="sidebar" :model="items" class="p-1 border-0 h-fit">
+	<Menu id="sidebar" :model="items" class="p-1 border-0 h-fit">
 		<template #start>
 			<div class="flex flex-col items-center py-2">
 				<span class="text-xl font-semibold">PLAN<span class="text-blue-300">TOGETHER</span></span>
-				<Image v-if="familyImageUrl" :src="familyImageUrl" alt="Image famille"
-					   image-class="rounded-xl"
-					   width="170"/>
+				<Image v-if="family.imageUrl" :src="family.imageUrl" alt="Image famille" image-class="rounded-xl"
+					width="170" />
 			</div>
 		</template>
 
 		<template #item="{ item, props }">
 			<router-link v-slot="{ href, navigate }" :to="item.route" custom>
 				<a :href="href" v-bind="props.action" @click="navigate">
-					<span :class="item.icon"/>
+					<span :class="item.icon" />
 					<span class="ml-2">{{ item.label }}</span>
 				</a>
 			</router-link>
@@ -22,23 +21,13 @@
 			<div class="flex flex-col gap-3">
 				<div class="inline-flex items-center justify-between">
 					<router-link class="inline-flex items-center px-3 py-2 gap-3 " to="/profile">
-						<Avatar v-if="user.imageUrl" :image="user.imageUrl"
-								shape="circle" size="small"/>
+						<Avatar v-if="user.imageUrl" :image="user.imageUrl" shape="circle" size="small" />
 						<Avatar v-else :label="userInitial" :style="`background-color: ${user.color}`"
-								class="font-semibold text-white" shape="circle" size="small"/>
+							class="font-semibold text-white" shape="circle" size="small" />
 						<span class="font-black">{{ user.name }}</span>
 					</router-link>
 					<span class="pi pi-sign-out pr-3" @click="logout"></span>
 				</div>
-
-				<Button icon="pi pi-user-plus" label="Créer une invitation" @click="createInvitation"/>
-				
-				<Dialog v-model:visible="dialogVisible" header="Code d'invitation">
-					<div class="flex flex-col items-center gap-3">
-						<p>{{ inviteCode }}</p>
-						<Button icon="pi pi-copy" label="Copier le code" @click="copyInviteCode"/>
-					</div>
-				</Dialog>
 			</div>
 		</template>
 	</Menu>
@@ -48,54 +37,26 @@
 import Menu from 'primevue/menu';
 import Image from "primevue/image";
 import Avatar from "primevue/avatar";
-import Button from "primevue/button";
-import Dialog from "primevue/dialog";
-import {createInvitationCode, getFamilyImage} from "@/services/familyServices.js";
 
 export default {
 	components: {
-		Menu, Image, Avatar, Button, Dialog
+		Menu, Image, Avatar
 	},
-	inject: ['token', 'user', 'logout'],
+	inject: ['token', 'user', 'family', 'logout'],
 	data() {
 		return {
 			items: [
-				{separator: true},
-				{label: 'Calendrier', icon: 'pi pi-calendar', route: '/events'},
-				{separator: true}
-			],
-			familyImageUrl: '',
-			isLoading: true,
-			dialogVisible: false,
-			inviteCode: ''
+				{ separator: true },
+				{ label: 'Calendrier', icon: 'pi pi-calendar', route: '/events' },
+				{ label: 'Famille', icon: 'pi pi-users', route: '/families' },
+				{ separator: true }
+			]
 		};
 	},
 	computed: {
 		userInitial() {
 			return this.user.name.charAt(0).toUpperCase();
 		}
-	},
-	methods: {
-		async createInvitation() {
-			try {
-				const response = await createInvitationCode(this.token);
-				this.inviteCode = response.inviteCode;
-				this.dialogVisible = true;
-			} catch (error) {
-				console.error('Erreur:', error);
-			}
-		},
-		copyInviteCode() {
-			navigator.clipboard.writeText(this.inviteCode).then(() => {
-				console.log('Code d\'invitation copié dans le presse-papiers');
-			}).catch(err => {
-				console.error('Erreur lors de la copie du code:', err);
-			});
-		}
-	},
-	async created() {
-		this.familyImageUrl = await getFamilyImage(this.token);
-		this.isLoading = false;
 	}
 }
 </script>
