0c422e2a1241c0a4a962abe8318bea91decb2271
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-11-13 14:46:49 -0500

    Ajouter css et mis à jour fichier locale  #7

--- frontend/src/pages/families/FamilyJoinForm.vue
+++ frontend/src/pages/families/FamilyJoinForm.vue
@@ -1,18 +1,18 @@
 <template>
-	<FloatingTitle/>
+	<FloatingTitle />
 
-	<div class="w-80">
+	<div class="w-80 pt-20">
 		<h1 class="text-3xl mb-8 text-center font-medium">{{ $t('joinFamily') }}</h1>
 		<form id="familyJoinForm" class="flex flex-col gap-5" @submit.prevent="submitJoinFamily">
 
 			<FloatLabel variant="on">
-				<InputText id="inviteCode" v-model.trim="inviteCode" class="w-full"/>
+				<InputText id="inviteCode" v-model.trim="inviteCode" class="w-full" />
 				<label for="inviteCode">{{ $t('inviteCode') }}</label>
 			</FloatLabel>
 
 			<Message v-if="errorMessage" class="error-message" severity="error">{{ errorMessage }}</Message>
 
-			<Button :disabled="isButtonDisabled" :label=" $t('confirmButton')" raised type="submit"/>
+			<Button :disabled="isButtonDisabled" :label="$t('confirmButton')" raised type="submit" />
 		</form>
 	</div>
 </template>
@@ -23,7 +23,7 @@ import Button from "primevue/button";
 import Message from 'primevue/message';
 import FloatLabel from "primevue/floatlabel";
 import FloatingTitle from "@/components/AppHeader.vue";
-import {joinFamily} from "@/services/familyServices.js";
+import { joinFamily } from "@/services/familyServices.js";
 
 export default {
 	components: {
@@ -59,5 +59,5 @@ export default {
 		}
 	}
 }
-;
+	;
 </script>
