ac1bc139c484fd87b30e0740e54953b27bce2b23
JSCoutu <151463469+JSCoutu@users.noreply.github.com>
2024-11-18 10:03:08 -0500

    feat(server): ajoute la gestion des fichiers statiques pour le frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan')
 const errorMiddleware = require('./middlewares/error/errorMiddleware');
+const path = require('path');
 
 const authRouter = require('./routes/authRouter');
 const familyRouter = require('./routes/familyRouter');
@@ -11,6 +12,8 @@ const eventRouter = require('./routes/eventRouter')
 
 const app = express();
 
+const frontendDistPath = path.join(__dirname, '../../frontend/dist');
+app.use(express.static(frontendDistPath));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
