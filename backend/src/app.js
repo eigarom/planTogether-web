fd7b0ecef5f3f7ccdba0505401fb56e488927b23
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-02 13:38:01 -0500

    ci: gestion api dans url pour azure

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,14 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
+// Supprime /api pour Azure
+app.use((req, res, next) => {
+	if (req.path.startsWith('/api/')) {
+		req.url = req.url.replace('/api', '');
+	}
+	next();
+});
+
 app.use('/auth', authRouter);
 app.use('/families', familyRouter);
 app.use('/families/my-family/members', memberRouter);
