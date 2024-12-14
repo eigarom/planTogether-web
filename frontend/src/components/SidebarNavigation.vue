dd314f0032a95243f929e240e15aed81ae816dde
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-14 13:05:14 -0500

    style: Modification sidebar

--- frontend/src/components/SidebarNavigation.vue
+++ frontend/src/components/SidebarNavigation.vue
@@ -1,40 +1,43 @@
 <template>
-	<Menu id="sidebar" :model="items" class="fixed left-5 z-40 p-4 h-[calc(100%-148px)] w-52">
-		<template #start>
-			<div class="flex flex-col gap-4 p-1">
-				<!--Informations de l'utilisateur-->
-				<router-link
-					class="inline-flex items-center gap-2 hover:bg-slate-100 rounded p-1"
-					to="/profile">
-					<Avatar
-						:image="user.imageUrl"
-						:label="!user.imageUrl ? userInitial : null"
-						:style="!user.imageUrl ? `background-color: ${user.color}` : null"
-						class="font-semibold text-white flex-shrink-0"
-						shape="circle"
-						size="small"
-					/>
-
-					<div class="flex flex-col justify-between w-[117px]">
-						<p class="text-nowrap overflow-hidden">{{ user.name }}</p>
-						<p class="text-xs">{{ family.name }}</p>
-					</div>
-				</router-link>
-
-				<!--Image de la famille-->
-				<div v-if="family.imageUrl" class="flex flex-col items-cente pb-1">
-					<Image :src="family.imageUrl" alt="Image famille" image-class="rounded-xl"/>
-				</div>
-			</div>
-		</template>
-
+	<Menu id="sidebar" :model="items" class="fixed left-5 z-40 p-2 h-[calc(100%-148px)] w-52">
 		<template #item="{ item, props }">
-			<router-link v-slot="{ href, navigate }" :to="item.route" custom>
-				<a :href="href" v-bind="props.action" @click="navigate">
-					<span :class="item.icon"/>
-					<span class="ml-2">{{ item.label }}</span>
-				</a>
-			</router-link>
+			<!-- Item du profil -->
+			<template v-if="item.route === '/profile'">
+				<router-link v-slot="{ href, navigate }" :to="item.route" custom>
+					<a :href="href" class="inline-flex items-center gap-2" v-bind="props.action" @click="navigate">
+						<Avatar
+							:image="user.imageUrl"
+							:label="!user.imageUrl ? userInitial : null"
+							:style="!user.imageUrl ? `background-color: ${user.color}` : null"
+							class="font-semibold text-white flex-shrink-0"
+							shape="circle"
+							size="small"
+						/>
+
+						<div class="flex flex-col justify-between w-[117px]">
+							<p class="text-nowrap overflow-hidden">{{ user.name }}</p>
+							<p class="text-xs">{{ family.name }}</p>
+						</div>
+					</a>
+				</router-link>
+			</template>
+
+			<!-- Item de l'image de la famille (non cliquable) -->
+			<template v-else-if="item.type === 'familyImage'">
+				<div class="flex flex-col items-center my-2">
+					<Image :src="family.imageUrl" alt="Image famille" image-class="rounded"/>
+				</div>
+			</template>
+
+			<!-- Autres items (calendrier, ma famille) -->
+			<template v-else>
+				<router-link v-slot="{ href, navigate }" :to="item.route" custom>
+					<a :href="href" v-bind="props.action" @click="navigate">
+						<span :class="item.icon"/>
+						<span class="ml-2">{{ item.label }}</span>
+					</a>
+				</router-link>
+			</template>
 		</template>
 	</Menu>
 </template>
@@ -57,10 +60,15 @@ export default {
 			return this.user.name.charAt(0).toUpperCase();
 		},
 		items() {
-			return [
+			let profileItem = {label: this.user.name, route: '/profile'};
+			let familyImageItem = this.family.imageUrl ? {type: 'familyImage'} : null;
+			let baseItems = [
 				{label: this.$t('calendarMenu'), icon: 'pi pi-calendar', route: '/events'},
 				{label: this.$t('familyMenu'), icon: 'pi pi-users', route: '/my-family'},
 			];
+			return familyImageItem
+				? [profileItem, familyImageItem, ...baseItems]
+				: [profileItem, ...baseItems];
 		}
 	}
 }
