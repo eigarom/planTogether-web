d5f4e63915aab3620d2ed234c411771438e8ccd4
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-16 15:11:18 -0500

    ci: modification deploiement azure

--- backend/src/queries/dbPool.js
+++ backend/src/queries/dbPool.js
@@ -6,7 +6,7 @@ const dbConfig = {
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    port: process.env.PGPORT,
     ssl: {
         rejectUnauthorized: false
     }
