7be1a9e0275137b6eacc1ceba268b5eabf3bb217
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-22 14:24:16 -0500

    fix(backend): connexion sécurisée à la base de données vercel

--- backend/src/queries/dbPool.js
+++ backend/src/queries/dbPool.js
@@ -6,7 +6,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    connectionString: process.env.DB_URL // Vercel
 };
 
 module.exports = new Pool(dbConfig);
