7c2c5bba0ef5fabb01aefa1678c48768b00ed54b
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-22 13:26:52 -0500

    db: renommage variables d'environnement pour vercel

--- backend/src/queries/dbPool.js
+++ backend/src/queries/dbPool.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const dbConfig = {
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 };
 
 module.exports = new Pool(dbConfig);
