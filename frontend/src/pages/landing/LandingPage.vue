b1556a68ef79cf14513b68cbaed2307ae4c56730
Romain Billot <96261466+eigarom@users.noreply.github.com>
2024-12-31 11:34:50 -0500

    feat(frontend): ajouts fonctionnalités dans landing page

--- frontend/src/pages/landing/LandingPage.vue
+++ frontend/src/pages/landing/LandingPage.vue
@@ -1,17 +1,20 @@
 <template>
 	<div class="fixed top-0 left-0 w-screen h-screen overflow-y-auto">
-		<!-- Section avec le dégradé et l'arc -->
 		<div
-			class="w-full h-[100vh]"
-			style="background: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
-             clip-path: ellipse(120% 60% at 80% 30%);"
-		>
+			class="flex flex-col sm:flex-row h-fit pt-24 sm:pt-32 bg-gradient-to-r from-blue-300 to-blue-100 clip-path-mobile clip-path-desktop">
 			<!-- Accroche -->
-			<div class="flex flex-col gap-5 px-10 sm:pl-28 pt-24 sm:pt-32 w-full">
+			<div class="flex flex-col gap-5 px-10 sm:pl-28 sm:w-[1200px]">
 
-				<p class="text-5xl font-bold">{{ $t('landingTitle') }}</p>
+				<p class="text-4xl sm:text-5xl font-bold">{{ $t('landingTitle') }}</p>
 
-				<p class="text-2xl font-thin">{{ $t('landingSubtitle') }}</p>
+				<p class="text-2xl font-light">{{ $t('landingSubtitle') }}</p>
+
+				<div class="flex flex-col gap-2">
+					<div v-for="feature in features" :key="feature.key" class="inline-flex items-center gap-5">
+						<i class="pi pi-check-square text-3xl text-green-600"/>
+						<p class="text-lg font-light">{{ $t(feature.translationKey) }}</p>
+					</div>
+				</div>
 
 				<Button as="router-link" class="flex items-center gap-2 w-36" to="/register">
 					<p>{{ $t('landingButton') }}</p>
@@ -20,16 +23,17 @@
 
 			</div>
 
-			<div class="relative w-[90vw] sm:w-[600px] mx-auto sm:ml-auto sm:mr-8 mt-8">
+			<!-- Images samples -->
+			<div class="relative w-[90vw] sm:w-full mx-auto sm:ml-auto sm:mr-8 mt-24 sm:mt-32 mb-48">
 				<img
 					:src="webSample"
 					alt="web sample"
-					class="w-full rounded absolute top-10 sm:top-16 left-0 shadow-lg"
+					class="w-full rounded shadow-lg"
 				/>
 				<img
 					:src="mobileSample"
 					alt="mobile sample"
-					class="w-[100px] sm:w-[180px] rounded absolute top-0 right-3 sm:right-4 shadow-lg"
+					class="w-[30%] rounded absolute top-[-40px] sm:top-[-64px] right-3 sm:right-4 shadow-xl"
 				/>
 			</div>
 		</div>
@@ -49,7 +53,12 @@ export default {
 	},
 	data() {
 		return {
-			mobileSampleEn, webSampleEn, mobileSampleFr, webSampleFr
+			mobileSampleEn, webSampleEn, mobileSampleFr, webSampleFr,
+			features: [
+				{key: 'calendar', translationKey: 'feature-calendar'},
+				{key: 'taskslists', translationKey: 'feature-taskslists'},
+				{key: 'shoppinglists', translationKey: 'feature-shoppinglists'}
+			]
 		};
 	},
 	computed: {
@@ -64,9 +73,13 @@ export default {
 </script>
 
 <style scoped>
-.container {
-	max-width: 1200px;
-	margin: 0 auto;
-	padding: 0 1rem;
+.clip-path-mobile {
+	clip-path: ellipse(150% 60% at 80% 20%);
+}
+
+@media (min-width: 640px) {
+	.clip-path-desktop {
+		clip-path: ellipse(120% 45% at 80% 30%);
+	}
 }
 </style>
